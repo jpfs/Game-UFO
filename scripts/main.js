ddd6f136731ea8b85bb9811f0d31030b2837f3c3
Joao Santos <jp.f.s@hotmail.com>
2024-12-30 18:41:02 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("ufoCanvas");
+
+canvas.width = 900;
+canvas.height = 750;
+
+//canvas automatic resizing
+function resize() {
+  const height = window.innerHeight - 20;
+
+  const ratio = canvas.width / canvas.height;
+  const width = height * ratio;
+
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+}
+
+window.addEventListener("load", resize, false);
