b1093b2440e67477da18fa83033e2d910193b100
Joao Santos <jp.f.s@hotmail.com>
2024-12-30 20:16:40 +0300

    commmit

---
+++ scripts/positionOpening.js
@@ -0,0 +1,36 @@
+// PositionOpening//
+function OpeningPosition() {}
+
+OpeningPosition.prototype.draw = function (play) {
+  //UFO Hunter
+  ctx.clearRect(0, 0, play.width, play.height);
+  ct1.font = "80px Comic Sans Ms";
+  ctx.textAlign = "center";
+  const gradient = ctx.createLinearGradient(
+    play.width / 2 - 180,
+    play.height / 2,
+    play.width / 2 + 180,
+    play.height / 2
+  );
+  gradient.addColorStop("0", "yellow");
+  gradient.addColorStop("0.5", "red");
+  gradient.addColorStop("1.0", "yellow");
+  ctx.fillStyle = gradient;
+  ctx.fillText("UFO HUNTER", play.width / 2, play.height / 2 - 70);
+
+  // Press "space" to start.
+  ctx.font = "40px Comic Sans Ms";
+  ctx.fillStyle = "#D7DF01";
+  ctx.fillText("Press 'Space' to start.", play.width / 2, play.height / 2);
+
+  //Game Controls
+  ctx.fillStyle = "#2e2f00";
+  ctx.fillText("Game Controls", play.width / 2, play.height / 2 + 210);
+  ctx.fillText("Left Arrow: Move Left", play.width / 2, play.height / 2 + 260);
+  ctx.fillText(
+    "Right Arrow: Move Right",
+    play.width / 2,
+    play.height / 2 + 300
+  );
+  ctx.fillText("Space: Fire", play.width / 2, play.height / 2 + 340);
+};
