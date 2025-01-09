785dc60f4ba4d78352042f95490221e7a1be7f17
Joao Santos <jp.f.s@hotmail.com>
2025-01-09 16:17:24 +0300

    commit

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,2 +1,13 @@
 // --- objects --- //
 
+function Objects() {}
+
+Objects.prototype.spaceship = function (x, y, spaceship_image) {
+  this.x = x;
+  this.y = y;
+  this.width = 34;
+  this.height = 28;
+  this.spaceship_image = spaceship_image;
+  this.spaceship_image.src = "images/ship.png";
+  return this;
+};
--- scripts/positionInGame.js
+++ scripts/positionInGame.js
@@ -1,10 +1,31 @@
 // --- positionInGame --- //
 
-function InGamePosition(settings, level) {}
+function InGamePosition(settings, level) {
+  this.settings = settings;
+  this.level = level;
+  this.object = null;
+  this.spaceship = null;
+}
+
+InGamePosition.prototype.update = function (play) {};
+
+InGamePosition.prototype.entry = function (play) {
+  this.spaceship_image = new Image();
+  this.object = new Objects();
+  this.spaceship = this.object.spaceship(
+    play.width / 2,
+    play.playBoundaries.bottom,
+    this.spaceship_image
+  );
+};
+
+InGamePosition.prototype.keyDown = function (play) {};
 
 InGamePosition.prototype.draw = function (play) {
   ctx.clearRect(0, 0, play.width, play.height);
-  ctx.font = "40px Comic Sans Ms";
-  ctx.filStyle = "#D7DF01";
-  ctx.fillText("We are in In Game Position", play.width / 2, play.height / 2);
+  ctx.drawImage(
+    this.spaceship_image,
+    this.spaceship.x - this.spaceship.width / 2,
+    this.spaceship.y - this.spaceship.height / 2
+  );
 };
