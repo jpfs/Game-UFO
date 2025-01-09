1f5ababacad76a30ab87570962cc6b4f82f063e9
Joao Santos <jp.f.s@hotmail.com>
2025-01-09 16:00:00 +0300

    commit

--- scripts/positionInGame.js
+++ scripts/positionInGame.js
@@ -1,2 +1,10 @@
 // --- positionInGame --- //
 
+function InGamePosition(settings, level) {}
+
+InGamePosition.prototype.draw = function (play) {
+  ctx.clearRect(0, 0, play.width, play.height);
+  ctx.font = "40px Comic Sans Ms";
+  ctx.filStyle = "#D7DF01";
+  ctx.fillText("We are in In Game Position", play.width / 2, play.height / 2);
+};
