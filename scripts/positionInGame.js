7838de4f3fc2cc97e5d6d1411ff59a381b80d593
Joao Santos <jp.f.s@hotmail.com>
2025-01-10 13:24:56 +0300

    commit

--- scripts/positionInGame.js
+++ scripts/positionInGame.js
@@ -3,11 +3,22 @@
 function InGamePosition(settings, level) {
   this.settings = settings;
   this.level = level;
+  this.upSec = this.settings.updateSeconds;
+  this.spaceshipSpeed = this.settings.spaceshipSpeed;
   this.object = null;
   this.spaceship = null;
 }
 
-InGamePosition.prototype.update = function (play) {};
+InGamePosition.prototype.update = function (play) {
+  //integer of LEFT 37, RIGHT 39;
+  if (play.pressedKeys[37]) {
+    this, (this.spaceship.x -= this.spaceshipSpeed * this.upSec);
+  }
+
+  if (play.pressedKeys[39]) {
+    this, (this.spaceship.x += this.spaceshipSpeed * this.upSec);
+  }
+};
 
 InGamePosition.prototype.entry = function (play) {
   this.spaceship_image = new Image();
@@ -19,7 +30,7 @@ InGamePosition.prototype.entry = function (play) {
   );
 };
 
-InGamePosition.prototype.keyDown = function (play) {};
+InGamePosition.prototype.keyDown = function (play, keyboardCode) {};
 
 InGamePosition.prototype.draw = function (play) {
   ctx.clearRect(0, 0, play.width, play.height);
