bc00a9cb3b51b371a1eff09ecb4a3bb03aef7675
Joao Santos <jp.f.s@hotmail.com>
2025-01-11 11:20:03 +0000

    commit

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,20 +1,31 @@
 // --- objects --- //
 
-function Objects() {
-};
+function Objects() {}
 
 Objects.prototype.spaceship = function (x, y, spaceship_image) {
-	this.x = x;
-	this.y = y;
-	this.width = 34;
-	this.height = 28;
-	this.spaceship_image = spaceship_image;
-	this.spaceship_image.src = "images/ship.png";
-	return this;
+  this.x = x;
+  this.y = y;
+  this.width = 34;
+  this.height = 28;
+  this.spaceship_image = spaceship_image;
+  this.spaceship_image.src = "images/ship.png";
+  return this;
 };
 
-Objects.prototype.bullet = function(x, y) {
-    this.x = x;
-    this.y = y;
-	return this;
+Objects.prototype.bullet = function (x, y) {
+  this.x = x;
+  this.y = y;
+  return this;
+};
+
+Objects.prototype.ufo = function (x, y, line, column, ufo_image) {
+  this.x = x;
+  this.y = y;
+  this.line = line;
+  this.column = column;
+  this.width = 32;
+  this.height = 24;
+  this.ufo_image = ufo_image;
+  this.ufo_image.src = "images/ufo.png";
+  return this;
 };
--- scripts/positionInGame.js
+++ scripts/positionInGame.js
@@ -1,88 +1,129 @@
 // --- positionInGame --- //
 
 function InGamePosition(setting, level) {
-    this.setting = setting;
-    this.level = level;
-    this.object = null;
-    this.spaceship = null;
-    this.bullets = [];
-    this.lastBulletTime = null;
+  this.setting = setting;
+  this.level = level;
+  this.object = null;
+  this.spaceship = null;
+  this.bullets = [];
+  this.lastBulletTime = null;
+  this.ufos = [];
 }
 
 InGamePosition.prototype.entry = function (play) {
-    this.spaceship_image = new Image();
-    this.upSec = this.setting.updateSeconds;
-    this.spaceshipSpeed = this.setting.spaceshipSpeed;
+  this.spaceship_image = new Image();
+  this.upSec = this.setting.updateSeconds;
+  this.spaceshipSpeed = this.setting.spaceshipSpeed;
+  this.ufo_image = new Image();
+  this.spaceship_image = new Image();
+  this.object = new Objects();
+  this.spaceship = this.object.spaceship(
+    play.width / 2,
+    play.playBoundaries.bottom,
+    this.spaceship_image
+  );
 
-    this.object = new Objects();
-    this.spaceship = this.object.spaceship((play.width / 2), play.playBoundaries.bottom, this.spaceship_image);
-}
+  //creating UFOs
+  const lines = this.setting.ufoLines;
+  const columns = this.setting.ufoColumns;
+  const ufosInitial = [];
+
+  let line, column;
+  for (line = 0; line < lines; line++) {
+    for (column = 0; column < columns; column++) {
+      this.object = new Objects();
+      let x, y;
+      x = play.width / 2 + column * 50 - (columns - 1) * 25;
+      y = play.playBoundaries.top + 30 + line * 30;
+      ufosInitial.push(this.object.ufo(x, y, line, column, this.ufo_image));
+      console.log(
+        "line: " + line + " column: " + column + " x:" + x + " y:" + y
+      );
+    }
+  }
+};
 
 InGamePosition.prototype.update = function (play) {
-    const spaceship = this.spaceship;
-    const spaceshipSpeed = this.spaceshipSpeed;
-    const upSec = this.setting.updateSeconds;
-    const bullets = this.bullets;
+  const spaceship = this.spaceship;
+  const spaceshipSpeed = this.spaceshipSpeed;
+  const upSec = this.setting.updateSeconds;
+  const bullets = this.bullets;
 
-    // Keyboard events
-    if (play.pressedKeys[37]) {
-        spaceship.x -= spaceshipSpeed * upSec;
-    }
-    if (play.pressedKeys[39]) {
-        spaceship.x += spaceshipSpeed * upSec;
-    }
-    if (play.pressedKeys[32]) {
-        this.shoot();
-    }
+  // Keyboard events
+  if (play.pressedKeys[37]) {
+    spaceship.x -= spaceshipSpeed * upSec;
+  }
+  if (play.pressedKeys[39]) {
+    spaceship.x += spaceshipSpeed * upSec;
+  }
+  if (play.pressedKeys[32]) {
+    this.shoot();
+  }
 
-    // Keep spaceship in 'Active playing field'
-    if (spaceship.x < play.playBoundaries.left) {
-        spaceship.x = play.playBoundaries.left;
-    }
-    if (spaceship.x > play.playBoundaries.right) {
-        spaceship.x = play.playBoundaries.right;
-    }
+  // Keep spaceship in 'Active playing field'
+  if (spaceship.x < play.playBoundaries.left) {
+    spaceship.x = play.playBoundaries.left;
+  }
+  if (spaceship.x > play.playBoundaries.right) {
+    spaceship.x = play.playBoundaries.right;
+  }
 
-    //  Moving bullets
-    for (let i = 0; i < bullets.length; i++) {
-        let bullet = bullets[i];
-        bullet.y -= upSec * this.setting.bulletSpeed;
-        // If our bullet flies out from the canvas, it will be cleared
-        if (bullet.y < 0) {
-            bullets.splice(i--, 1);
-        }
+  //  Moving bullets
+  for (let i = 0; i < bullets.length; i++) {
+    let bullet = bullets[i];
+    bullet.y -= upSec * this.setting.bulletSpeed;
+    // If our bullet flies out from the canvas, it will be cleared
+    if (bullet.y < 0) {
+      bullets.splice(i--, 1);
     }
-}
+  }
+};
 
 InGamePosition.prototype.draw = function (play) {
-    // draw Spaceship
-    ctx.clearRect(0, 0, play.width, play.height);
-    ctx.drawImage(this.spaceship_image, this.spaceship.x - (this.spaceship.width / 2), this.spaceship.y - (this.spaceship.height / 2));
+  // draw Spaceship
+  ctx.clearRect(0, 0, play.width, play.height);
+  ctx.drawImage(
+    this.spaceship_image,
+    this.spaceship.x - this.spaceship.width / 2,
+    this.spaceship.y - this.spaceship.height / 2
+  );
 
-    // draw Bullets 
-    ctx.fillStyle = '#ff0000';
-    for (let i = 0; i < this.bullets.length; i++) {
-        let bullet = this.bullets[i];
-        ctx.fillRect(bullet.x - 1, bullet.y - 6, 2, 6);
-    }
-}
-
+  // draw Bullets
+  ctx.fillStyle = "#ff0000";
+  for (let i = 0; i < this.bullets.length; i++) {
+    let bullet = this.bullets[i];
+    ctx.fillRect(bullet.x - 1, bullet.y - 6, 2, 6);
+  }
+  //draw UFOs
+  for (let i = 0; i < this.ufos.length; i++) {
+    let ufo = this.ufos[i];
+    ctx.drawImage(
+      this.ufo_image,
+      ufo.x - ufo.width / 2,
+      ufo.y - ufo.height / 2
+    );
+  }
+};
 
 InGamePosition.prototype.shoot = function () {
-    console.log(((new Date()).getTime() - this.lastBulletTime));
-    if (this.lastBulletTime === null || ((new Date()).getTime() - this.lastBulletTime) > (this.setting.bulletMaxFrequency)) {
-        console.log('We are shooting!');
-        this.object = new Objects();
-        this.bullets.push(this.object.bullet(this.spaceship.x, this.spaceship.y - this.spaceship.height / 2, this.setting.bulletSpeed));
-        this.lastBulletTime = (new Date()).getTime();
-    }
+  console.log(new Date().getTime() - this.lastBulletTime);
+  if (
+    this.lastBulletTime === null ||
+    new Date().getTime() - this.lastBulletTime > this.setting.bulletMaxFrequency
+  ) {
+    console.log("We are shooting!");
+    this.object = new Objects();
+    this.bullets.push(
+      this.object.bullet(
+        this.spaceship.x,
+        this.spaceship.y - this.spaceship.height / 2,
+        this.setting.bulletSpeed
+      )
+    );
+    this.lastBulletTime = new Date().getTime();
+  }
 };
 
 InGamePosition.prototype.keyDown = function (play, keyboardCode) {
-    // more code
-}
-
-
-
-
-
+  // more code
+};
